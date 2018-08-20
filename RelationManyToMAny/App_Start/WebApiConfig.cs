b8256e58bef121e0b5d14ed6d0bda7ee6213cf65
Герций Елена <gertsij.elena@gmail.com>
2018-08-20 12:27:49 +0300

    add WebApiConfig

---
+++ RelationManyToMAny/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WebApiConfig.cs" company="Artisans">
+//     Copyright (c) Artisans. All rights reserved.
+// </copyright>
+// <author>Elena Gertsiy</author>
+//-----------------------------------------------------------------------
+
+namespace RelationManyToMAny
+
+{  
+    using System.Net.Http.Headers;
+    using System.Web.Http;
+
+    /// <summary>
+    /// The Configuration of WebApi
+    /// </summary>
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// Register 
+        /// </summary>
+        /// <param name="config">Parameters for Configuration</param>
+        public static void Register(HttpConfiguration config)
+        {
+           config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute(
+               name: "ApiByActionAndId",
+               routeTemplate: "api/{controller}/{action}/{id}",
+               defaults: new { action = "Get" });
+        }
+    }
+}
